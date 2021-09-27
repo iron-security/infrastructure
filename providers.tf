13f7c915b7e70171a4c25945537a4563e242587d
hazcod <niels@ironpeak.be>
2021-09-27 10:45:51 +0200

    chore: fmt

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= v1.0.6"
 
   backend "gcs" {
-    bucket = "ironsecurity-terraform-state"
-    prefix = "terraform/state"
+    bucket       = "ironsecurity-terraform-state"
+    prefix       = "terraform/state"
     access_token = env.GCP_TOKEN
   }
 
